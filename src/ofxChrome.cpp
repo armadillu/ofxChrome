c2449f08b59e5c6e41eb839611ae3c07818c9b67
Oriol Ferrer Mesia <oriol@uri.cat>
2017-07-27 21:25:11 -0400

    doh

--- src/ofxChrome.cpp
+++ src/ofxChrome.cpp
@@ -339,7 +339,7 @@ bool ofxChrome::loadPage(string url, bool fullPage){
 
 	ofLogNotice("ofxChrome") << "loadPage(\"" << url << "\");";
 	browserFetch(url, "", fullPage);
-	return true
+	return true;
 }
 
 
