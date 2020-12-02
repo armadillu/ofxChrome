6b034bd747eb83cdf1d13a1fd3b027caa8f240f9
Oriol Ferrer Mesia <oriol@uri.cat>
2020-12-02 12:57:33 +0100

    ci

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -7,7 +7,7 @@ echo "Executing ci/install.sh"
 export OF_ROOT=~/openFrameworks
 export OF_ADDONS=$OF_ROOT/addons
 
-ADDONS=" armadillu/ofxExternalProcess armadillu/ofxTimeMeasurements robotconscience/ofxLibwebsockets "
+ADDONS=" armadillu/ofxExternalProcess armadillu/ofxTimeMeasurements armadillu/ofxLibwebsockets "
 
 cd $OF_ADDONS
 
